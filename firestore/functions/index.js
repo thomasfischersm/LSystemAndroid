e5a227844faa9836ece25f1e33a94fecc39151c2
thomasfischersm <thomas@playposse.com>
2017-11-27 18:51:04 -0800

    Add cloud function to score like counts.

--- firestore/functions/index.js
+++ firestore/functions/index.js
@@ -1,13 +1,41 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Fuck off the bird!");
-});
+exports.updateLikeCount = functions.firestore
+    .document('likes/{likeId}').onWrite((event) => {
 
+        // Determine operation type.
+        var exists = event.data.exists;
+        var hasExisted = (event.data.previous != null) && event.data.previous.exists;
+        var isCreate = (exists && !hasExisted);
+        var isDelete = (!exists && hasExisted);
+        console.log('Determined operations: ' + exists + ' ' + hasExisted + ' ' + isCreate + ' ' + isDelete);
+
+        // Determine rule set id.
+        if (isCreate) {
+            console.log('isCreate');
+            var ruleSetId = event.data.data().ruleSetId;
+        } else if (isDelete) {
+            console.log('isDelete');
+            var ruleSetId = event.data.previous.data().ruleSetId;
+        } else {
+            console.log('Unpexected operation! ' + exists + ' ' + hasExisted);
+        }
+        console.log('Selected rule set is: ' + ruleSetId)
+
+        // Update like count.
+        return admin.firestore()
+            .collection('likes')
+            .where('ruleSetId', '==', ruleSetId)
+            .get()
+            .then(query => {
+                console.log('like count is: ' + query.size);
+                var likeCount = query.size;
+                return admin.firestore()
+                            .collection('ruleSets')
+                            .doc(ruleSetId)
+                            .set({likeCount: likeCount}, { merge: true });
+            });
+    });
